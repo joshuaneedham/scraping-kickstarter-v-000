d765bd18873d29461798bf4caf2ddcad7ed7e15e
Joshua Needham <joshuabneedham@gmail.com>
2017-11-29 16:54:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,4 +2,9 @@
 require 'Nokogiri'
 def create_project_hash
   # write your code here
+  attr_accessor :title, :image_link, :description, :location, :percent_funded
+
+  def initialize(title)
+    @title = title    
+  end
 end
