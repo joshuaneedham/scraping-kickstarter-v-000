5df4fdececd54023120e95a0cf863dfa0d2c0a46
Joshua Needham <joshuabneedham@gmail.com>
2017-11-29 16:57:32 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,10 @@
 # require libraries/modules here
 def create_project_hash
   # write your code here
-  attr_accessor :title
+  attr_accessor :project_title
   @@projects = {}
-  def initialize(title)
-    @title = title
+  def initialize(project_title)
+    @project_title = project_title
     @@projects << self
   end
 
