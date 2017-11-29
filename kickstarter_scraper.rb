64a9706daf827bc4641f6fd7a1afcd3dea68f923
Joshua Needham <joshuabneedham@gmail.com>
2017-11-29 16:55:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,9 +2,13 @@
 require 'Nokogiri'
 def create_project_hash
   # write your code here
-  attr_accessor 
+  attr_accessor
   @@projects = {}
   def initialize(title)
-    @title = title    
+    @title = title
+  end
+
+  def projects
+    @@projects
   end
 end
